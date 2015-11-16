14b8febae6012896567b7d54efe4f078bba79018
Grayson Lorenz <lunar_furor@hotmail.com>
2015-11-16 17:02:38 -0600

    Update Galbey.java

--- Galbey.java
+++ Galbey.java
@@ -1,38 +1,22 @@
-import java.util.Date;
-import java.util.Random;
-
 public class Galbey {
 	//methods
-	public static void act(Global newGlobal){
-		//Make and start threads to do the dew
-		Thread move = new Thread(new MovementMaster(newGlobal));
-		Thread speak = new Thread(new VoiceMaster(newGlobal));
-		
-		move.start();
-		speak.start();
-	}
-
 	
 	public static void main(String[] args){
 		//initialize stuff
 		Global masterGlobal = new Global();
-		Random rand = new Random();
 		boolean loop = true;
 		
 		//start the word processing which connects to the channel.
 		Thread read = new Thread(new WordProcessor(masterGlobal));
-		read.start();
+		Thread move = new Thread(new MovementMaster(masterGlobal));
+		Thread speak = new Thread(new VoiceMaster(masterGlobal));
 		
-		//main loop
+		read.start();
+		move.start();
+		speak.start();
+		
 		while(loop){
-			try {
-				Thread.sleep(1000 * (3 + rand.nextInt(1))); //wait 
-				System.out.println(new Date().toString() + " - " + masterGlobal.toString()); //show me how global is looking
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			
-			act(masterGlobal); //do something
+						
 		}
 	}
 }
