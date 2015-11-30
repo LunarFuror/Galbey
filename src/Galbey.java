752a2eecacac8908a7cf499b1a450598969fcc77
Grayson Lorenz <lunar_furor@hotmail.com>
2015-11-30 08:16:45 -0600

    no longer used

--- src/Galbey.java
+++ src/Galbey.java
@@ -1,22 +1,17 @@
-public class Galbey {
-	//methods
-	
-	public static void main(String[] args){
-		//initialize stuff
-		Global masterGlobal = new Global();
-		boolean loop = true;
-		
-		//start the word processing which connects to the channel.
-		Thread read = new Thread(new WordProcessor(masterGlobal));
-		Thread move = new Thread(new MovementMaster(masterGlobal));
-		Thread speak = new Thread(new VoiceMaster(masterGlobal));
-		
-		read.start();
-		move.start();
-		speak.start();
-		
-		while(loop){
-						
-		}
-	}
-}
+//public class Galbey {
+//	//methods
+//	
+//	public static void main(String[] args){
+//		//initialize stuff
+//		Global masterGlobal = new Global();
+//		boolean loop = true;
+//		
+//		//start the word processing which connects to the channel.
+//		Thread read = new Thread(new WordProcessor(masterGlobal));
+//		read.start();
+//		
+//		while(loop){
+//						
+//		}
+//	}
+//}
