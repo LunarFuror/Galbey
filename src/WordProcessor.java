30ce647c4d38464b3e22948837cacaaf0f0fa32c
Grayson Lorenz <lunar_furor@hotmail.com>
2015-11-30 08:15:08 -0600

    Update WordProcessor.java

--- src/WordProcessor.java
+++ src/WordProcessor.java
@@ -1,102 +1,85 @@
 import java.io.IOException;
+import java.time.LocalTime;
+import java.util.Scanner;
+
 import org.pircbotx.Configuration;
+import org.pircbotx.MultiBotManager;
 import org.pircbotx.PircBotX;
-import org.pircbotx.cap.EnableCapHandler;
 import org.pircbotx.exception.IrcException;
-import org.pircbotx.hooks.ListenerAdapter;
-import org.pircbotx.hooks.events.ConnectEvent;
-import org.pircbotx.hooks.events.JoinEvent;
-import org.pircbotx.hooks.types.GenericMessageEvent;
+import org.pircbotx.hooks.managers.ThreadedListenerManager;
 
 @SuppressWarnings("rawtypes")
-public class WordProcessor extends ListenerAdapter implements Runnable {
-	static PircBotX bot;
-	Global thisGlobal;
-	
-	//CONSTRUCTORS GO HERE
-	public WordProcessor(){
-		super();
-	}
-	
-	public WordProcessor(Global newGlobal){
-		super();
-		thisGlobal = newGlobal;
-	}
+public class WordProcessor{
 
-	//NORMAL METHODS
-
-	//LISTENER OVERRIDE METHODS
-	public void onConnect(ConnectEvent event) {
-		System.out.println("Connected!");
-	}
-
-	// lets us know when we've entered the channel
-	public void onJoin(JoinEvent event) {
-		System.out.println("Joined!");
-		event.respond("Hello!");
-	}
-
-	// parse messages for commands and emotions
-	public void onGenericMessage(GenericMessageEvent event) throws InterruptedException {
-		// When someone says ?helloworld respond with "Hello World"
-		if (event.getMessage().toLowerCase().contains("!galbey")) {
-			event.respond("Yes boss?");
-		}
-
-		if (event.getMessage().toLowerCase().contains("@lunargalbey")) {
-			event.respond("I was built to read, not to lead.");
-		}
-
-		if (event.getMessage().toLowerCase().contains("#joy")) {
-			System.out.println("joy");
-			thisGlobal.incrimentJoy();
-		}
-
-		if (event.getMessage().toLowerCase().contains("#sadness")) {
-			thisGlobal.incrimentSadness();
-		}
-
-		if (event.getMessage().toLowerCase().contains("#anger")) {
-			thisGlobal.incrimentAnger();
-		}
-
-		if (event.getMessage().toLowerCase().contains("#humor")) {
-			thisGlobal.incrimentHumor();
-		}
-
-		if (event.getMessage().toLowerCase().contains("#dissapointment")) {
-			thisGlobal.incrimentDissapointment();
-		}
-
-		if (event.getMessage().toLowerCase().contains("#scared")) {
-			thisGlobal.incrimentScared();
-		}
-	}
-
-	@Override
-	public void run() {
-		@SuppressWarnings("unchecked")
-		Configuration<PircBotX> configuration = new Configuration.Builder<PircBotX>().setAutoNickChange(false)
-			.addCapHandler(new EnableCapHandler("twitch.tv/membership"))
-	
-			.setName("lunargalbey") // Set the nick of the bot. CHANGE IN YOUR CODE
-			.setServerHostname("irc.twitch.tv") // Join the freenode network
-			.setServerPort(6667).setServerPassword("oauth:sapooyzgzir30i31mzd8u9xdvbgt97")
-			.addAutoJoinChannel("#lunargalbey") // Join the official pircbotx channel
-	
-			.addListener(new WordProcessor(thisGlobal)) // Add our listener that will be called on Events
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) throws IOException, IrcException, InterruptedException {
+		Global thisGlobal = new Global();
+		final Scanner sc = new Scanner(System.in);
+		ThreadedListenerManager man = new ThreadedListenerManager();
+		PircBotListener theListener = new PircBotListener(thisGlobal);
+		man.addListener(theListener);
+		
+		Configuration<PircBotX> config = new Configuration.Builder<PircBotX>()
+			.setName("lunargalbey")
+			.setServerPassword("oauth:sapooyzgzir30i31mzd8u9xdvbgt97")
+			.addAutoJoinChannel(thisGlobal.getChannel())
+			.setServerHostname("irc.twitch.tv")
+			.setServerPort(6667)
+			.setListenerManager(man)
 			.buildConfiguration();
+		
+		MultiBotManager<PircBotX> manager = new MultiBotManager<PircBotX>();
+		PircBotX theBot = new PircBotX(config);
+		manager.addBot(theBot);
+		manager.start();
+		Thread th = new Thread(){
+			public void run() {
 
-		// Create our bot with the configuration
-		bot = new PircBotX(configuration);
-		// Connect to the server
-		System.out.println("Connecting...");
-		try {
-			bot.startBot();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (IrcException e) {
-			e.printStackTrace();
+				System.out.println("Bots should be started");
+				if (sc.hasNext())
+				{
+					String in = sc.next();
+					if (in.equals("Stop"))
+						return;
+				}
+			}
+		};
+		th.start();
+		System.out.println("Bots should be started");
+		
+		//MAIN TIMING LOOP
+		LocalTime timeCheck = LocalTime.now();
+		LocalTime timedResponse = LocalTime.now();
+		int seconds = 0;
+		while (th.isAlive())
+		{
+			if (theBot.isConnected())
+			{
+				System.out.println("Here I am with a time check");
+				if (theListener.getLastMessageRespondedTime().plusSeconds(30).isBefore(LocalTime.now()))
+				{
+					man.dispatchEvent(new TimeEvent(theBot, theBot.getUserBot().getChannels().first()));
+				}
+				if (timeCheck.plusSeconds(10).isBefore(LocalTime.now()))
+				{
+					seconds += 10;
+					System.out.println("Seconds:" + seconds);
+					if(seconds == 30){seconds = 0;}
+					timeCheck = LocalTime.now();
+				}
+				if (timedResponse.plusSeconds(10).isBefore(LocalTime.now()))
+				{
+					man.dispatchEvent(new TimeEvent(theBot, theBot.getUserBot().getChannels().first(),"TimedResponse"));
+				}
+			}
+			Thread.sleep(100);
 		}
+		
+		try {
+			manager.stopAndWait();
+		} catch (InterruptedException e) {
+			System.out.println("Something went horribly wrong" + e.getMessage());
+		}
+		System.out.println("AllBots Stopped");
 	}
 }
